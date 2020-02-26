ea65f22b0fccbf479b5702998c7c911b2a3caebf
Alex Merced <alexmerced@alexmerced.com>
2020-02-26 14:13:35 -0500

    updates

--- src/data.js
+++ src/data.js
@@ -1,105 +1,100 @@
 export const menu = [
-    {
-        label: 'Youtube',
-        url: 'https://www.youtube.com/channel/UCoc4UCEetAt3htM3hV1dQgQ'
-    },
-    {
-        label: 'GitHub',
-        url: 'https://github.com/alexmercedcoder'
-    },
-    {
-        label: 'LinkedIn',
-        url: 'http://www.linkedin.com/in/alexmerced'
-    },
-    {
-        label: 'Podcast',
-        url: 'https://open.spotify.com/show/1kMcquypdIElTu4Nu98XPM'
-    },
-    {
-        label: 'Slack',
-        url: 'https://join.slack.com/t/amwebdev/shared_invite/enQtNzc4NDA3MDU3MDE0LTZjNjIyZmQ3MzA5Y2Q3MWUwZjk3NTIyYjliOThlMWFjNTFkYWM1OGUxN2Y3NGExNGVhOGIzZTg0YTJjZTk5NDA'
-    },
-    {
-        label: 'Discord',
-        url: 'https://discord.gg/hQ2AySW'
-    },
-    {
-        label: 'Spectrum',
-        url: 'https://spectrum.chat/devnursery'
-    },
-]
+  {
+    label: "Youtube",
+    url: "https://www.youtube.com/channel/UCoc4UCEetAt3htM3hV1dQgQ"
+  },
+  {
+    label: "GitHub",
+    url: "https://github.com/alexmercedcoder"
+  },
+  {
+    label: "LinkedIn",
+    url: "http://www.linkedin.com/in/alexmerced"
+  },
+  {
+    label: "Podcast",
+    url: "https://open.spotify.com/show/1kMcquypdIElTu4Nu98XPM"
+  },
+  {
+    label: "Slack",
+    url:
+      "https://join.slack.com/t/amwebdev/shared_invite/enQtNzc4NDA3MDU3MDE0LTZjNjIyZmQ3MzA5Y2Q3MWUwZjk3NTIyYjliOThlMWFjNTFkYWM1OGUxN2Y3NGExNGVhOGIzZTg0YTJjZTk5NDA"
+  },
+  {
+    label: "Discord",
+    url: "https://discord.gg/hQ2AySW"
+  },
+  {
+    label: "Spectrum",
+    url: "https://spectrum.chat/devnursery"
+  }
+];
 
 export const projects = [
-    {
-        image: 'https://i.imgur.com/vWCc9ot.png',
-        github: 'https://github.com/AlexMercedCoder/devNurserySvelteBlogApp',
-        live: 'https://main.devnursery.com/',
-        title: 'devNursery'
-    },
-    {
-        image: 'https://i.imgur.com/sRcGAXe.png',
-        github: 'https://github.com/AlexMercedCoder/SavvyBackendAPI',
-        live: 'https://savvyhotspots.netlify.com/',
-        title: 'Savvy Hotspots'
-    },
-    {
-        image: 'https://i.imgur.com/MLcgfsf.png',
-        github: 'https://github.com/Jordan-Morales/betterboxd',
-        live: 'https://betterboxd.herokuapp.com/',
-        title: 'BetterBoxD'
-    },
-    {
-        image: 'http://alexmercedcoder.com/evevol.png',
-        github: 'https://github.com/AlexMercedCoder/Unit2EveVol',
-        live: 'https://shrouded-beach-63520.herokuapp.com/',
-        title: 'EveVol - Event/Volunteers'
-    },
-    {
-        image: 'https://i.imgur.com/vWCc9ot.png',
-        github: 'https://github.com/AlexMercedCoder/devNurserySvelteBlogApp',
-        live: 'https://main.devnursery.com/',
-        title: 'devNursery'
-    },
-    {
-        image: 'http://alexmercedcoder.com/amblog.png',
-        github: 'https://github.com/AlexMercedCoder/AlexMercedExpressBlog',
-        live: 'https://fierce-waters-95519.herokuapp.com/index',
-        title: 'Full Stack Blog'
-    },
-    {
-        image: 'http://alexmercedcoder.com/pet.jpg',
-        github: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        live: 'https://alexmercedcoder.github.io/petadoptionapp/',
-        title: 'Pet Adoption App'
-    },
-    {
-        image: 'http://alexmercedcoder.com/calc.png',
-        github: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        live: 'https://optimistic-lamarr-1dc751.netlify.com/',
-        title: 'Calculator'
-    },
-    {
-        image: 'http://alexmercedcoder.com/todo.png',
-        github: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        live: 'https://xenodochial-beaver-d27eca.netlify.com/',
-        title: 'Todo Lists'
-    },
-    {
-        image: 'http://alexmercedcoder.com/bill.jpg',
-        github: 'https://stoic-stonebraker-80dd15.netlify.com/',
-        live: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        title: 'Bill & Ted TicTacToe'
-    },
-    {
-        image: 'http://alexmercedcoder.com/space.jpg',
-        github: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        live: 'https://dazzling-einstein-0e4817.netlify.com/',
-        title: 'Space Battle'
-    },
-    {
-        image: 'http://alexmercedcoder.com/grave.jpg',
-        github: 'https://github.com/AlexMercedCoder/GABootcampProjects',
-        live: 'https://vibrant-franklin-2cd867.netlify.com/',
-        title: 'Graveyard Landscaper'
-    },
-]
+  {
+    image: "https://i.imgur.com/vWCc9ot.png",
+    github: "https://github.com/AlexMercedCoder/devNurserySvelteBlogApp",
+    live: "https://main.devnursery.com/",
+    title: "devNursery"
+  },
+  {
+    image: "https://i.imgur.com/sRcGAXe.png",
+    github: "https://github.com/AlexMercedCoder/SavvyBackendAPI",
+    live: "https://savvyhotspots.netlify.com/",
+    title: "Savvy Hotspots"
+  },
+  {
+    image: "https://i.imgur.com/MLcgfsf.png",
+    github: "https://github.com/Jordan-Morales/betterboxd",
+    live: "https://betterboxd.herokuapp.com/",
+    title: "BetterBoxD"
+  },
+  {
+    image: "http://alexmercedcoder.com/evevol.png",
+    github: "https://github.com/AlexMercedCoder/Unit2EveVol",
+    live: "https://shrouded-beach-63520.herokuapp.com/",
+    title: "EveVol - Event/Volunteers"
+  },
+  {
+    image: "http://alexmercedcoder.com/amblog.png",
+    github: "https://github.com/AlexMercedCoder/AlexMercedExpressBlog",
+    live: "https://fierce-waters-95519.herokuapp.com/index",
+    title: "Full Stack Blog"
+  },
+  {
+    image: "http://alexmercedcoder.com/pet.jpg",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://alexmercedcoder.github.io/petadoptionapp/",
+    title: "Pet Adoption App"
+  },
+  {
+    image: "http://alexmercedcoder.com/calc.png",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://optimistic-lamarr-1dc751.netlify.com/",
+    title: "Calculator"
+  },
+  {
+    image: "http://alexmercedcoder.com/todo.png",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://xenodochial-beaver-d27eca.netlify.com/",
+    title: "Todo Lists"
+  },
+  {
+    image: "http://alexmercedcoder.com/bill.jpg",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://stoic-stonebraker-80dd15.netlify.com/",
+    title: "Bill & Ted TicTacToe"
+  },
+  {
+    image: "http://alexmercedcoder.com/space.jpg",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://dazzling-einstein-0e4817.netlify.com/",
+    title: "Space Battle"
+  },
+  {
+    image: "http://alexmercedcoder.com/grave.jpg",
+    github: "https://github.com/AlexMercedCoder/GABootcampProjects",
+    live: "https://vibrant-franklin-2cd867.netlify.com/",
+    title: "Graveyard Landscaper"
+  }
+];
